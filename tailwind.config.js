05a6b8f336ad6c34e77c1f45bf48403104853846
Idriss Douiri <drisspennywise@gmail.com>
2025-01-04 09:28:19 +0000

    adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        moderateBlue: { DEFAULT: "hsl(238, 40%, 52%)" },
+        softRed: { DEFAULT: "hsl(358, 79%, 66%)" },
+        lightGrayishBlue: { DEFAULT: "hsl(239, 57%, 85%)" },
+        paleRed: { DEFAULT: "hsl(357, 100%, 86%)" },
+        darkBlue: { DEFAULT: "hsl(212, 24%, 26%)" },
+        grayishBlue: { DEFAULT: "hsl(211, 10%, 45%)" },
+        lightGray: { DEFAULT: "hsl(223, 19%, 93%)" },
+        veryLightGray: { DEFAULT: "hsl(228, 33%, 97%)" },
+      },
+    },
   },
   plugins: [],
 };
